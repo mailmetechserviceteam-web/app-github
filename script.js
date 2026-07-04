0688ceaaa707fda9d78a3c96895039af2c449083
yuvaan raikwar <mailme.techserviceteam@gmail.com>
2026-07-04 14:42:02 +0530

    Added Portfolio Website

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Portfolio Started...");
+var typed = new Typed("#typing", {
+
+    strings: [
+        "IT Support Engineer",
+        "Full Stack Java Developer",
+        "Hardware & Networking Engineer",
+        "Field Engineer"
+    ],
+
+    typeSpeed: 70,
+
+    backSpeed: 40,
+
+    backDelay: 1500,
+
+    loop: true
+
+});
