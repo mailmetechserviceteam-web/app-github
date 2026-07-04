5de9b713c55bacbf5ac7243a281a09728d137782
yuvaan raikwar <mailme.techserviceteam@gmail.com>
2026-07-04 22:03:47 +0530

    fix

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 console.log("Portfolio Started...");
-
 var typed = new Typed("#typing", {
+
     strings: [
         "IT Support Engineer",
+        "Full Stack Java Developer",
+        "Chat process support Engineer",
         "Field Engineer",
-        "Chat Support Engineer",
-        "Full Stack Java Developer"
-        ],
+        "Hardware & Networking Engineer"
+        
+    ],
+
     typeSpeed: 70,
+
     backSpeed: 40,
-    backDelay: 800,
-    startDelay: 200,
-    loop: true,
-    showCursor: true,
-    cursorChar: "|",
-    smartBackspace: true
+
+    backDelay: 1500,
+
+    loop: true
+
 });
